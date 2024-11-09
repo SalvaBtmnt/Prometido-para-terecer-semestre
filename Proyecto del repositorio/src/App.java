f6ecbcfc5fc8453baa07a903fffe1b554eae5c3f
Ca-va Drinks <sbustamante602@uvaq.edu.mx>
2024-11-08 22:34:34 -0600

    Update App.java

--- Proyecto del repositorio/src/App.java
+++ Proyecto del repositorio/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("HOLA A TODOS, POR FIN MI REPOSITORIO EN GIT!!");
+        //just messing around with code jeje
     }
 }
